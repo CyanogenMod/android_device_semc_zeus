15d64f72fe1ebea0e99a3b74e417554d99528b46
freexperia <jerpelea@gmail.com>
2012-01-15 13:22:34 +0200

    semc_zeus: updated device with latest changes

---
+++ BoardConfig.mk
@@ -0,0 +1,7 @@
+-include device/semc/zeus-common/BoardConfigCommon.mk
+-include vendor/semc/zeus/BoardConfigVendor.mk
+
+TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
+
+TARGET_OTA_ASSERT_DEVICE := R800i,R800a,R800at,zeus
+
