340c4f9f1456e25e30694caf4cd698c088a3c9c7
freexperia <jerpelea@gmail.com>
2012-03-02 04:15:02 +0200

    semc: enables sensor build and prop cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 -include device/semc/zeus-common/BoardConfigCommon.mk
 -include vendor/semc/zeus/BoardConfigVendor.mk
 
-BUILD SEMC SENSORS := false
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
