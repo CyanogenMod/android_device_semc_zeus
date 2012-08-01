d581db8ae77c2cf22c8ea272bbb79a4e3c9fc0b8
freexperia <jerpelea@gmail.com>
2012-08-01 07:04:31 +0300

    zeus: updates for jb

--- cm.mk
+++ cm.mk
@@ -1,22 +1,17 @@
+# Inherit device configuration
+$(call inherit-product, device/semc/zeus/full_zeus.mk)
+
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := R800i
-
-TARGET_BOOTANIMATION_NAME := vertical-480x854
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/semc/zeus/device_zeus.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := zeus
-PRODUCT_NAME := cm_zeus
-PRODUCT_BRAND := SEMC
-PRODUCT_MODEL := Xperia Play
-
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800i BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
+
+TARGET_BOOTANIMATION_NAME := vertical-480x854
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_zeus
+PRODUCT_DEVICE := zeus
---
+++ zeus.mk
@@ -0,0 +1,30 @@
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/semc/zeus/zeus-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/semc/zeus/overlay
+
+$(call inherit-product, device/semc/mogami-common/mogami.mk)
+
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+PRODUCT_AAPT_CONFIG := normal hdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES += \
+    device/semc/zeus/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/zeus/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
+    device/semc/zeus/prebuilt/hw_config.sh:system/etc/hw_config.sh \
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
+    device/semc/zeus/prebuilt/bootrec-device:root/sbin/bootrec-device \
+    device/semc/zeus/recovery.fstab:root/recovery.fstab
+
+$(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
+
+#LCD DENSITY
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    com.qc.hdmi_out=false
