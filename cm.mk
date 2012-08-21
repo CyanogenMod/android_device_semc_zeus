7f3aa2db3c10d1530196d5f5fd282cfa66e4261e
freexperia <jerpelea@gmail.com>
2012-08-21 18:34:59 +0300

    zeus: new bootanimations

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800i BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-480x854
+TARGET_BOOTANIMATION_NAME := vertical-480
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_zeus
