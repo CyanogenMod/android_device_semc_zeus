90359926a0b2cddb6082d4247afcf51758621f0e
Giulio Cervera <giulio.cervera@gmail.com>
2012-09-10 11:36:19 +0200

    zeus: touchpad fixes
    thank's to santiis2010
    http://forum.xda-developers.com/showthread.php?t=1862722
    
    Change-Id: I1b7bbbf4c91f0a8686f76cca49a20016a4d153c6

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := R800i
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800i BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND="Sony Ericsson" PRODUCT_NAME="Xperia Play" BUILD_FINGERPRINT=SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x854
 
