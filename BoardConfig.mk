6840c68db0dd2a623878856e9fc95a6fb1d18745
Michael Bestas <mikeioannina@gmail.com>
2012-12-22 21:01:13 +0200

    zeus: remove prebuilt kernel
    We build kernel from source since long time
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ifd61a7da97eb0b02b307d765d356cfb17bcba5e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,6 @@
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := cyanogen_zeus_defconfig
-#TARGET_PREBUILT_KERNEL := device/semc/zeus/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
 TARGET_RECOVERY_INITRC := device/semc/zeus/recovery/init.rc
@@ -19,4 +18,3 @@ TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
 BOARD_USES_STEREO_HW_SPEAKER := true
 
 TARGET_OTA_ASSERT_DEVICE := R800i,R800a,R800at,zeus
-
